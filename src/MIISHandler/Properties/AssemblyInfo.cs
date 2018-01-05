ea5e2af902c25a8f7fb4c9762f9cc0a5c7803e22
José M. Alarcón <jmalarcon@jasoft.org>
2018-01-05 13:43:58 +0100

    Bumped version to 2.0

--- src/MIISHandler/Properties/AssemblyInfo.cs
+++ src/MIISHandler/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MIISHandler")]
-[assembly: AssemblyCopyright("Copyright © JM Alarcón  2017 - www.jasoft.org")]
+[assembly: AssemblyCopyright("JM Alarcón  2017-2018 - JMAlarcon - www.jasoft.org - Open Source")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
