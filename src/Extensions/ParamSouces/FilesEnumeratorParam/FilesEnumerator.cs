089440be9af4e7b02b7a7ddbe289edcad2e17369
JM Alarcón <jmalarcon@jasoft.org>
2019-07-23 21:31:53 +0200

    Add new FM source to get all the tags from published posts

--- src/Extensions/ParamSouces/FilesEnumeratorParam/FilesEnumerator.cs
+++ src/Extensions/ParamSouces/FilesEnumeratorParam/FilesEnumerator.cs
@@ -11,7 +11,7 @@ namespace FilesEnumeratorParam
     /// <summary>
     /// A helper enum to specify the sort order of the resulting files
     /// </summary>
-    public enum SortDirection
+    internal enum SortDirection
     {
         desc = 0,
         asc = 1
@@ -20,7 +20,7 @@ namespace FilesEnumeratorParam
     /// <summary>
     /// Helper class to retrieve files from folder in different ways
     /// </summary>
-    public static class FilesEnumerator
+    internal static class FilesEnumeratorHelper
     {
         //Valid file extensions - Only MIIS files (.md or .mdh)
         private static readonly string[] VALID_EXTS = new string[] { MarkdownFile.MARKDOWN_DEF_EXT, MarkdownFile.HTML_EXT };
@@ -41,14 +41,14 @@ namespace FilesEnumeratorParam
             return folderAbsPath;
         }
 
+
         /// <summary>
         /// Returns all the files in an specific folder, excluding files begining with "_" and including only .md or .mdh files.
         /// </summary>
         /// <param name="folderPath">The full path to the folder that contains the files</param>
         /// <param name="topFolderOnly">If true the results will include files into subfolders too</param>
-        /// <param name="sortdirection">The sort direction ordering the files by date.</param>
-        /// <returns></returns>
-        public static IEnumerable<MarkdownFile> GetAllFilesFromFolder(string folderPath, bool topFolderOnly, SortDirection sortdirection)
+        /// <returns>Returns files in the dafult order they have in disk. NO further ordering is done</returns>
+        public static IEnumerable<MarkdownFile> GetAllFilesFromFolder(string folderPath, bool topFolderOnly)
         {
             if (!Directory.Exists(folderPath))
             {
@@ -61,16 +61,80 @@ namespace FilesEnumeratorParam
             DirectoryInfo di = new DirectoryInfo(folderPath);
 
             var allFiles = (from file in di.EnumerateFiles("*.*", sfo)
-                            //Include only MIIS files and exclude files that start with "_" or with a default name (index or default)
+                                //Include only MIIS files and exclude files that start with "_" or with a default name (index or default)
                             where !file.Name.StartsWith("_") && VALID_EXTS.Contains(file.Extension.ToLower()) && !EXCLUDED_FILE_NAMES.Contains(Path.GetFileNameWithoutExtension(file.Name).ToLower())
                             select new MarkdownFile(file.FullName)
                            );
+
+            return allFiles;
+        }
+
+        /// <summary>
+        /// Returns all the files in an specific folder, excluding files begining with "_" and including only .md or .mdh files.
+        /// </summary>
+        /// <param name="folderPath">The full path to the folder that contains the files</param>
+        /// <param name="topFolderOnly">If true the results will include files into subfolders too</param>
+        /// <param name="sortdirection">The sort direction ordering the files by date.</param>
+        /// <returns></returns>
+        public static IEnumerable<MarkdownFile> GetAllFilesFromFolder(string folderPath, bool topFolderOnly, SortDirection sortdirection)
+        {
+            var allFiles = GetAllFilesFromFolder(folderPath, topFolderOnly);
 
             var allFilesSorted = (sortdirection == SortDirection.desc) ? 
                                  allFiles.OrderByDescending<MarkdownFile, DateTime>(f => f.Date) : //First the newest
                                  allFiles.OrderBy<MarkdownFile, DateTime>(f => f.Date); //First the oldest
 
             return allFilesSorted;
+        }
+
+        public static IEnumerable<MIISFile> OnlyPublished(IEnumerable<MarkdownFile> allFiles)
+        {
+            return from file in allFiles
+                   where file.IsPublished
+                   select new MIISFile(file);
+        }
+
+        /// <summary>
+        /// Adds cache dependencies for the source file depending on the current files being processed
+        /// </summary>
+        /// <param name="currentFile"></param>
+        /// <param name="folderPath"></param>
+        /// <param name="allFiles"></param>
+        public static void AddCacheDependencies(MIISFile currentFile, string folderPath, IEnumerable<MarkdownFile> allFiles)
+        {
+            //Establish the processed folder as a caching dependency for the current file this FM souce is working on
+            currentFile.AddFileDependency(folderPath);
+            //If any of the files has a (publishing) date later than now, then add that as a cache dependency to refresh the listings at that point
+            double maxDateSecs = ((from f in allFiles select f.Date).Max<DateTime>() - DateTime.Now).TotalSeconds;
+            if (maxDateSecs > 0)
+                currentFile.SetMaxCacheValidity(maxDateSecs);
+        }
+
+        //Other generic aux methods
+
+        /// <summary>
+        /// Determines if a string value (normally got from params or Front-Matter)
+        /// representes a "true" value or not.
+        /// Valid "true" values are: "1", "true" and "yes"
+        /// </summary>
+        /// <param name="val">The value to be checked</param>
+        /// <returns>true if is a truthy value according to the criteria</returns>
+        internal static bool IsTruthy(string val)
+        {
+            val = val.ToLower();
+            return (val == "1" || val == "true" || val == "yes");
+        }
+
+        /// <summary>
+        /// Determines if a string value (normally got from params or Front-Matter)
+        /// representes a "false" value or not.
+        /// Valid "false" values are: "0", "false" and "no"
+        /// </summary>
+        /// <param name="val">The value to be checked</param>
+        /// <returns>true if is a truthy value according to the criteria</returns>
+        internal static bool IsFalsy(string val)
+        {
+            return (val == "0" || val == "false" || val == "no");
         }
     }
 }
--- src/Extensions/ParamSouces/FilesEnumeratorParam/TagsFolderParamSrc.cs
+++ src/Extensions/ParamSouces/FilesEnumeratorParam/TagsFolderParamSrc.cs
@@ -7,26 +7,25 @@ using FilesEnumeratorParam;
 
 namespace MIISHandler.FMSources
 {
-    public class FilesFromFolderParamSrc : IFMSource
+    public class TagsFolderParamSrc : IFMSource
     {
-        string IFMSource.SourceName => "FilesFromFolder";
+        string IFMSource.SourceName => "TagsFromFolder";
 
         /// <summary>
-        /// Returns all the files in the specified folder order by descending date.
+        /// Returns all the distinct tags available in the files of the specified folder order by tag name (ascending).
         /// Includes only .md or .mdh files
         /// Excludes files with a name starting with "_", with the name "index" or "default".
         /// By default includes only the files directly inside the folder, not in subfolders
         /// 
-        /// Syntax: FilesFromFolder folderName topFolderOnly(true*/false or 1/0) sortDirection (desc*/asc)
+        /// Syntax: TagsFromFolder folderName topFolderOnly(true*/false or 1/0)
         ///
         /// Examples: 
         /// FilesFromFolder folderName
         /// FilesFromFolder folderName false
-        /// FilesFromFolder folderName true asc
         /// </summary>
         /// <param name="currentFile"></param>
         /// <param name="parameters"></param>
-        /// <returns>A List of file information objects that can be used in Liquid tags</MIISFile></returns>
+        /// <returns>An array of strings with tag names in files, that can be used in Liquid tags</MIISFile></returns>
         object IFMSource.GetValue(MIISFile currentFile, params string[] parameters)
         {
             //Expects these parameters (* indicates the default value):
@@ -43,30 +42,32 @@ namespace MIISHandler.FMSources
             try
             {
                 string sTopOnly = parameters[1].ToLower();
-                topOnly = FilesEnumeratorHelper.IsTruthy(sTopOnly); //If the second parameter is "false" or "0", then use topOnly files
+                topOnly = topOnly = FilesEnumeratorHelper.IsTruthy(sTopOnly);   //If the second parameter is "false" or "0", then use topOnly files
             }
             catch { }
 
-            //Use the correct sort order, if any (third parameter)
-            SortDirection sd = SortDirection.desc;    //Default value
-            try
-            {
-                sd = (SortDirection)Enum.Parse(typeof(SortDirection), parameters[2]);
-            }
-            catch { }
 
-            //Get al files in the folder (and subfolders if indicated), ordered by date desc
-            IEnumerable<MarkdownFile> allFiles = FilesEnumeratorHelper.GetAllFilesFromFolder(folderPath, topOnly, sd);
-
-            //FILE CACHING
-            FilesEnumeratorHelper.AddCacheDependencies(currentFile, folderPath, allFiles);
+            //Get al files in the folder (and subfolders if indicated), without specific ordering (we don't need it and it'll save some processing time)
+            IEnumerable<MarkdownFile> allFiles = FilesEnumeratorHelper.GetAllFilesFromFolder(folderPath, topOnly);
 
             //Filter only those that are published
             var publishedFilesProxies = FilesEnumeratorHelper.OnlyPublished(allFiles);
 
-            //TODO: Filter by tag and category from QueryString params
+            //Get all the tags in the published files (if any)
+            HashSet<string> hTags = new HashSet<string>();
+            //int max = publishedFilesProxies.Count() - 1;
+            //for(int i = 0; i<=max; i++)
+            foreach (MIISFile mf in publishedFilesProxies)
+            {
+                hTags.UnionWith(mf.Tags);
+            }
 
-            return publishedFilesProxies.ToList<MIISFile>();
+            //FILE CACHING
+            FilesEnumeratorHelper.AddCacheDependencies(currentFile, folderPath, allFiles);
+
+            //Return tags
+            return hTags.ToArray<string>();
+
         }
     }
 }
